f58d0571f40cbd22ee4a51e2a7f8c95ef176abf2
Tresor Diwata <dev@tresordiwata.com>
2025-04-29 13:58:19 +0200

    Correction

---
+++ app/api/budgets/[id]/route.ts
@@ -0,0 +1,39 @@
+import { NextResponse, NextRequest } from "next/server";
+
+export async function GET(Request: NextRequest) {
+  try {
+    return NextResponse.json({}, { status: 201 });
+  } catch (error: any) {
+    return NextResponse.json({ error: error.toString() }, { status: 501 });
+  }
+}
+
+export async function POST(Request: NextRequest) {
+  const {} = await Request.json();
+
+  try {
+    return NextResponse.json({}, { status: 201 });
+  } catch (error: any) {
+    return NextResponse.json({ error: error.toString() }, { status: 501 });
+  }
+}
+
+export async function PUT(Request: NextRequest) {
+  const {} = await Request.json();
+
+  try {
+    return NextResponse.json({}, { status: 201 });
+  } catch (error: any) {
+    return NextResponse.json({ error: error.toString() }, { status: 501 });
+  }
+}
+
+export async function DELETE(Request: NextRequest) {
+  const {} = await Request.json();
+
+  try {
+    return NextResponse.json({}, { status: 201 });
+  } catch (error: any) {
+    return NextResponse.json({ error: error.toString() }, { status: 501 });
+  }
+}
