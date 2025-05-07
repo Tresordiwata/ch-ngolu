d566de22c01b0a16f7d17f61888af805e24dbe48
Tresor Diwata <dev@tresordiwata.com>
2025-05-07 06:48:47 +0200

    update du 07_05_2025-2

---
+++ app/utils/getRangeOfDate.ts
@@ -0,0 +1,16 @@
+import moment from "moment";
+
+const getRangeOfDates = (dtS: String, dtE: String) => {
+  const dates = [];
+  let currentDate = moment(`${dtS}`).clone();
+
+  while (currentDate.isSameOrBefore(moment(`${dtE}`))) {
+    dates.push(currentDate.format("YYYY-MM-DD"));
+    currentDate.add(1, "days"); // Ajouter un jour
+  }
+  dates.unshift("Rubrique");
+
+  return dates;
+};
+
+export default getRangeOfDates;
